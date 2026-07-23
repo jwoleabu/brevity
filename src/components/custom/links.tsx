8081d5a7e711185b62b5fff5f318b652b856e62c
Joseph <jwoleabu@gmail.com>
2026-07-23 15:09:25 +0100

    feat: add domain-specific icons for links

--- src/components/custom/links.tsx
+++ src/components/custom/links.tsx
@@ -1,18 +1,39 @@
 import { Link } from "@/lib/workspace";
 import { Globe } from "lucide-react";
+import { getDomainWithoutSuffix } from "tldts";
+import type { JSX } from "react";
+import GithubIcon from "@/assets/brand/github.svg?react";
+import LinkedinIcon from "@/assets/brand/linkedin.svg?react";
 
 type LinkProps = {
   links: Link[];
 };
+
+const domainIconMap = new Map<string, JSX.Element>([
+  ["github", <GithubIcon width={20} height={20} className="ml-2" />],
+  ["linkedin", <LinkedinIcon width={20} height={20} className="ml-2"/>],
+]);
+
+function UrlToIcon(url: string): JSX.Element {
+  const domain = getDomainWithoutSuffix(url);
+  return (
+    domainIconMap.get(domain ?? "") ?? (
+      <Globe size={20} className="text-indigo-400 ml-2" />
+    )
+  );
+}
 
 export default function Links({ links }: LinkProps) {
   console.log("howdy", links);
   return (
     <div className="flex flex-col gap-1.5 w-full">
       {links.map((link) => (
-        <div key={link.id} className="flex items-center gap-1.5 bg-muted rounded-lg p-1 text-sm transition-colors ease-in duration-75 hover:bg-indigo-100 cursor-pointer active:bg-muted">
-            <Globe size={14} className="text-indigo-400 ml-1.5"/>
-            <p>{link.url}</p>
+        <div
+          key={link.id}
+          className="flex items-center gap-2 bg-muted rounded-lg p-1 text-sm transition-colors ease-in duration-75 hover:bg-indigo-100 cursor-pointer active:bg-muted"
+        >
+          {UrlToIcon(link.url)}
+          <p>{link.url}</p>
         </div>
       ))}
     </div>
