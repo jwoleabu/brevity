04c6f1004b8a6499bfebde49d6612483418ce38b
Joseph <jwoleabu@gmail.com>
2026-08-08 23:52:23 +0100

    feat: add Firefox data collection permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,30 +1,37 @@
-import { defineConfig } from "wxt";
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
-  srcDir: "src",
-  vite: () => ({
-    plugins: [tailwindcss(), svgr()],
-  }),
-  manifestVersion: 3,
-  manifest: {
-    name: "Brevity",
-    description:
-      "Speed up your job applications by writing only what you need to.",
-    version: "1.0",
-    action: {},
-    content_scripts: [
-      {
-        matches: ["<all_urls>"],
-        js: ["content.ts"],
-      },
-    ],
-  },
-  autoIcons: {
-    baseIconPath: resolve("src", "assets/icon.svg"),
-  },
+	modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
+	srcDir: "src",
+	vite: () => ({
+		plugins: [tailwindcss(), svgr()],
+	}),
+	manifestVersion: 3,
+	manifest: {
+		name: "Brevity",
+		description:
+			"Speed up your job applications by writing only what you need to.",
+		version: "1.0",
+		action: {},
+		content_scripts: [
+			{
+				matches: ["<all_urls>"],
+				js: ["content.ts"],
+			},
+		],
+		browser_specific_settings: {
+			gecko: {
+				data_collection_permissions: {
+					required: ["none"],
+				},
+			},
+		},
+	},
+	autoIcons: {
+		baseIconPath: resolve("src", "assets/icon.svg"),
+	},
 });
